c47851437a95a79f5771272271b7660481f8e896
Ime Samuel <samuel.imex@gmail.com>
2025-05-25 17:54:56 +0100

    added structured logging interceptor to log incoming HTTP request

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/logging.interceptor';
+import { v4 as uuidv4 } from 'uuid';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use((req, res, next) => {
+    req['requestId'] = uuidv4();
+    next();
+  });
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+ await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
